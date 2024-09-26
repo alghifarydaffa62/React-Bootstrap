9dbb33fc0326cefdee13daefba9e80fc11ca66ed
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-09-26 20:31:58 +0700

    about services, works

--- corporate/src/App.js
+++ corporate/src/App.js
@@ -1,6 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Appheader from './component/header';
 import Hero from './component/hero';
+import About from './component/about';
+import Services from './component/services';
+import Works from './component/works';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       </header>
       <main>
         <Hero/>
+        <About/>
+        <Services/>
+        <Works/>
       </main>
     </div>
   );
